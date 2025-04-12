e9621ec5a6a76048ab4c1e891ae9288014f5c85d
Alex Candido <alex.candido.tec@gmail.com>
2025-04-12 17:06:07 -0300

    iniciando estrutura do projeto

---
+++ app/(tasks)/tasks/(routes)/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function TasksScreen() {
+  return (
+    <View>
+      <Text>TasksScreen</Text>
+    </View>
+  );
+}
---
+++ app/(tasks)/tasks/_layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function TasksLayout() {
+  return (
+    <View>
+      <Text>TasksLayout</Text>
+    </View>
+  );
+}
